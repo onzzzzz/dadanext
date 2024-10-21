e65ca3557e617b30002a6154826d82826e95735c
onzzzzz <ninaonz.shop@gmail.com>
2024-10-21 10:52:19 +0100

    home page nav

---
+++ const/const .js
@@ -0,0 +1,12 @@
+const  categories = [
+    "Abstract",
+    "Cartoon",
+    "ThreeD",
+    "FanArt",
+    "Manga",
+    "All",
+    "Retro",
+    "PixelArt",
+    "Space"
+]
+export default categories;
